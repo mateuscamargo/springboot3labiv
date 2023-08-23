24c56ad864a2fa225a04e1d9c4217029444b1a2f
Mateus Camargo <mateuscamargolima@gmail.com>
2023-08-23 00:07:08 +0000

    Feat: Controller e Interface

--- src/main/java/br/gov/sp/fatec/springboot3labiv/service/UsuarioService.java
+++ src/main/java/br/gov/sp/fatec/springboot3labiv/service/UsuarioService.java
@@ -1,5 +1,8 @@
 package br.gov.sp.fatec.springboot3labiv.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,23 +10,33 @@ import br.gov.sp.fatec.springboot3labiv.entity.Usuario;
 import br.gov.sp.fatec.springboot3labiv.repository.UsuarioRepository;
 
 @Service
-public class UsuarioService {
+public class UsuarioService implements IUsuarioService{
     
     @Autowired
     private UsuarioRepository usuarioRepo;
 
     public Usuario novoUsuario(Usuario usuario) {
-        
-        if (usuario == null ||
-            usuario.getNome() == null ||
-            usuario.getNome().isBlank() ||
-            usuario.getSenha() == null||
-            usuario.getSenha().isBlank()) {
+        if(usuario == null ||
+                usuario.getNome() == null ||
+                usuario.getNome().isBlank() || 
+                usuario.getSenha() == null ||
+                usuario.getSenha().isBlank()) {
             throw new IllegalArgumentException("Usuário com atributos inválidos!");
         }
-        
         return usuarioRepo.save(usuario);
-
     }
+
+    public List<Usuario> buscarTodosUsuarios() {
+        return usuarioRepo.findAll();
+    }
+
+    public Usuario buscarPorId(Long id) {
+        Optional<Usuario> usuarioOp = usuarioRepo.findById(id);
+        if(usuarioOp.isEmpty()) {
+            throw new IllegalArgumentException("Usuario nao encontrado!");
+        }
+        return usuarioOp.get();
+    }
+
 
 }
